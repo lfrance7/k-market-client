f345f6a1802d0ab014b035ca0454919972f9cc19
jaehyun.kwon <jaehyun.kwon@kt.com>
2021-11-03 17:11:40 +0900

    vercel 적용

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? "https://kwon-market-server.herokuapp.com/" : "http://localhost:8090";
+export const API_URL =
+    process.env.NODE_ENV === "production"
+        ? "https://kwon-market-server.herokuapp.com/"
+        : "http://localhost:8090";
